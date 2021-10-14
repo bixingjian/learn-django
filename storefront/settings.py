4b0745ed6197513aee5fe5b12328cdd3d8ac6672
bixingjian <bixingjian19@163.com>
2021-10-14 22:38:58 +0800

    add running custom sql

--- storefront/settings.py
+++ storefront/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import storefront
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,8 +88,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "Bxj20020405"
     }
 }
 
