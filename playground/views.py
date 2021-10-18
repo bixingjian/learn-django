768250895aab209266e7a3efe9c2335a79d895e7
bixingjian <bixingjian19@163.com>
2021-10-18 14:49:44 +0800

    update(exp_wra, generic_rel)

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,14 @@
+from django.contrib.contenttypes.models import ContentType
 from django.core import exceptions
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
+from django.db.models.fields import DecimalField
+from django.db.models import Q, F, Value, Func, ExpressionWrapper
+from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Avg, Sum, Max, Min
+from django.http import HttpResponse
+from django.shortcuts import render
 from store.models import Product, Customer, OrderItem, Order
+from tags.models import TaggedItem, Tag
 
 
 def say_hello(request):
@@ -32,7 +36,29 @@ def say_hello(request):
     # queryset = Product.objects.select_related("collection").all() #如果另一端的是一个(另一端是外键). product对应一个collection, collection可以有多个product
     # queryset = Product.objects.prefetch_related("promotions").all() #如果另一端是多个(manytomany, 外键的多的那一端) 
     # queryset = Product.objects.prefetch_related("promotions").select_related("collection").all() #preload的操作可以串在一起, 顺序没有要求
-    queryset = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
+    # queryset = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
 
+    # res_dict = Product.objects.filter(collection__id=1).aggregate(count=Count("id"), min_price=Min("unit_price"))
+    # res_dict = Order.objects.filter(customer__id=1).aggregate(count=Count("id"))
+    # res_dict = Product.objects.filter(collection__id=3).aggregate(min_price=Min("unit_price"), avg_price=Avg("unit_price"), max_price=Max("unit_price"))
 
-    return render(request, 'hello.html', {'name': 'Mosh', "orders": queryset})
+    # queryset = Customer.objects.annotate(is_new=Value(True))
+    # queryset = Customer.objects.annotate(new_id=F("id")+1)
+
+    # queryset = Customer.objects.annotate(full_name=Func(F("first_name"), Value(" "), F("last_name"), function="concat"))
+    # queryset = Customer.objects.annotate(full_name=Concat("first_name", Value(" "), "last_name"))
+
+    # queryset = Customer.objects.annotate(orders_count=Count("order")) #使用order 不是 order_set
+
+    # discounted_price = ExpressionWrapper(F("unit_price") * 0.8, output_field=DecimalField()) 
+    # queryset = Product.objects.annotate(discounted_price = discounted_price)
+    # queryset = Customer.objects.annotate(orders_count=Count("order")).filter(orders_count__gt=5)
+
+    content_type = ContentType.objects.get_for_model(Product) #找到product的content_type_id
+    queryset = TaggedItem.objects \
+        .select_related("tag") \
+        .filter(
+            content_type=content_type, 
+            object_id=1
+        ) 
+    return render(request, 'hello.html', {'name': 'Mosh', "tags": list(queryset)})
